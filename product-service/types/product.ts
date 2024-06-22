427b094fce0f77b3dc6e148fff93d29183df7497
Anton Paskanny <anton.paskanny@gmail.com>
2024-06-22 13:13:11 +0200

    feat: update product type

--- product-service/types/product.ts
+++ product-service/types/product.ts
@@ -2,19 +2,13 @@ export type ProductItem = {
     id: string;
     title: string;
     description: string;
-    platform: string;
-    genre: string;
     price: number;
-    rating: number;
 };
 
 export type ProductItemFull = {
     id: number;
     title: string;
     description: string;
-    platform: string;
-    genre: string;
     price: number;
-    rating: number;
     count: number;
 };
