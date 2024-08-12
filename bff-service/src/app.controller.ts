2d4e0240f29c12214550d0df62f9d9911a509f30
Anton Paskanny <anton.paskanny@gmail.com>
2024-08-12 13:18:28 +0200

    feat: update app controller and main ts files

--- bff-service/src/app.controller.ts
+++ bff-service/src/app.controller.ts
@@ -1,12 +1,53 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Controller,
+  Req,
+  Res,
+  HttpException,
+  HttpStatus,
+  All,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { ConfigService } from '@nestjs/config';
+import axios from 'axios';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly configService: ConfigService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @All('/:serviceName/*')
+  async proxyRequest(@Req() req: Request, @Res() res: Response) {
+    const { serviceName } = req.params;
+    const recipientUrl = this.configService.get<string>(
+      `${serviceName.toUpperCase()}_SERVICE_URL`,
+    );
+
+    if (!recipientUrl) {
+      throw new HttpException(
+        '[BFF Controller] Cannot process request',
+        HttpStatus.BAD_GATEWAY,
+      );
+    }
+
+    const targetPath = req.url.replace(`/${serviceName}`, '');
+
+    try {
+      const response = await axios({
+        method: req.method,
+        url: `${recipientUrl}${targetPath}`,
+        headers: req.headers,
+        data: req.body,
+        params: req.query,
+      });
+
+      res.status(response.status).send(response.data);
+    } catch (error) {
+      if (error.response) {
+        res.status(error.response.status).send(error.response.data);
+      } else {
+        res
+          .status(HttpStatus.INTERNAL_SERVER_ERROR)
+          .send('[BFF Conrtoller] Internal server error');
+      }
+    }
   }
 }
--- bff-service/src/main.ts
+++ bff-service/src/main.ts
@@ -3,6 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // BFF service will be called from a front-end app hosted on a different origin (EBS).
+  // So the CORS should be enabled.
+  app.enableCors();
+
+  app.use((req, res, next) => {
+    console.log(`[BFF Bootstrap] Original url: ${req.originalUrl}`);
+    next();
+  });
+
+  await app.listen(process.env.PORT || 4000);
 }
+
 bootstrap();
